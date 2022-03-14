8f6f5be49ca0675732e4e433391630e77f727a40
Simon Siah <simon.siah@sofax.com>
2022-03-14 12:30:37 +0800

    update

--- frontend/src/components/Layout/Footer.js
+++ frontend/src/components/Layout/Footer.js
@@ -6,7 +6,7 @@ class Footer extends Component {
     return (
       <footer className='footer-container'>
         <span>
-          &copy; {year} - Simon Siah. <span> Checkout source at </span>
+          &copy; {year} - Simon Siah. <span> Source at </span>
           <a href='https://github.com/htsiah/todo-list' target='_blank'>
             https://github.com/htsiah/todo-list
           </a>{' '}
